a85f93fe61d92d026c78b6ae80d64504ef70f407
ClearingPath <13512093@std.stei.itb.ac.id>
2014-06-16 22:41:31 +0700

    latest commit

---
+++ Source/Assets/HealthScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handle hitpoints and damages
+/// </summary>
+public class HealthScript : MonoBehaviour
+{
+	/// <summary>
+	/// Total hitpoints
+	/// </summary>
+	public int hp = 1;
+	
+	/// <summary>
+	/// Enemy or player?
+	/// </summary>
+	public bool isEnemy = true;
+	
+	/// <summary>
+	/// Inflicts damage and check if the object should be destroyed
+	/// </summary>
+	/// <param name="damageCount"></param>
+	public void Damage(int damageCount)
+	{
+		hp -= damageCount;
+		
+		if (hp <= 0)
+		{
+			// Dead!
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Source/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Simply moves the current game object
+/// </summary>
+public class MoveScript : MonoBehaviour
+{
+	// 1 - Designer variables
+	
+	/// <summary>
+	/// Object speed
+	/// </summary>
+	public Vector2 speed = new Vector2(10, 10);
+	
+	/// <summary>
+	/// Moving direction
+	/// </summary>
+	public Vector2 direction = new Vector2(-1, 0);
+
+	private Vector2 tempMov;
+
+	private Vector2 movement;
+
+	private bool moved = false;
+
+	void Update()
+	{
+		// 2 - Movement
+		if (transform.position.x < -100) {
+						Destroy (gameObject);
+				} else {
+						if ((movement.x == 0 && movement.y == 0) && moved) {
+								movement = tempMov;
+						}
+			else{
+				moved = true;
+				movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
+			}
+				}
+	}
+	
+	void FixedUpdate()
+	{
+		// Apply movement to the rigidbody
+		rigidbody2D.velocity = movement;
+	}
+
+	void OnCollisionEnter2D (Collision2D hit){
+		string name = hit.gameObject.name;
+		tempMov = movement;
+		movement = new Vector2 (0, 0);
+		if (name == "player") {
+						if (direction.Equals (new Vector2 (-1, 0))) {
+								direction.Set (0, 1);
+						} else if (direction.Equals (new Vector2 (0, 1))) {
+								direction.Set (1, 0);
+						} else if (direction.Equals (new Vector2 (1, 0))) {
+								direction.Set (0, -1);
+						} else if (direction.Equals (new Vector2 (0, -1))) {
+								direction.Set (-1, 0);
+						}
+				}
+	}
+}
